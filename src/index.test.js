44e408bbdbda9803e9d91874e17c7e19beb5c4ff
Michael Brett <mdouglasbrett@icloud.com>
2017-12-14 18:34:50 +0000

    Add jsdom test  and watch task

--- src/index.test.js
+++ src/index.test.js
@@ -7,10 +7,13 @@ describe('Our first test', () => {
 });
 
 describe('index.html', () =>  {
-  it('should say hello', () => {
+  it('should say hello', (done /* Async, requires callback */) => {
     const index = fs.readFileSync('./src/index.html', 'utf-8');
     jsdom.env(index, (err, window) => {
-
+      const h1 = window.document.getElementsByTagName('h1')[0];
+      expect(h1.innerHTML).to.equal('Hello world!');
+      window.close();
+      done();
     });
   })
 });
