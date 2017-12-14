7e3fbafa8433ff0d8b4be157bebf603563ada4ba
Michael Brett <mdouglasbrett@icloud.com>
2017-12-14 08:29:39 +0000

    Add eslint config and eslint-watch
    Also adds some eslint-disable examples

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import numeral from 'numeral';
 import './index.css';
 
+/* eslint-disable no-console */
+
 const courseValue = numeral(1000).format('$0,0.00');
 // debugger <--- set a breakpoint for soucemap
 console.log(`I would pay ${courseValue} for this awesome course!`);
