50220598c98879e22071f8c41d63b0229d5df1ec
Michael Brett <mdouglasbrett@icloud.com>
2017-12-18 08:51:34 +0000

    Add rough api and update index.html

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -17,7 +17,15 @@ app.use(require('webpack-dev-middleware')(compiler, {
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../src/index.html'));
-})
+});
+
+app.get('/users', (req, res) => {
+  res.json([
+    {"id": 1, "firstName": "Bob", "lastName": "Smith", "email": "bob@gmail.com"},
+    {"id": 2, "firstName": "Dave", "lastName": "Jones", "email": "dave@hotmail.com"},
+    {"id": 3, "firstName": "Sarah", "lastName": "Davies", "email": "sarah@iclooud.com"}
+  ]);
+});
 
 app.listen(port, err => {
   if (err) {
--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
-import numeral from 'numeral';
-import './index.css';
+import {getUsers} from './api/userApi';
 
-/* eslint-disable no-console */
-
-const courseValue = numeral(1000).format('$0,0.00');
-// debugger <--- set a breakpoint for soucemap
-console.log(`I would pay ${courseValue} for this awesome course!`);
+// Populate table of users via API call
+getUsers().then(result => {
+  let usersBody = '';
+  result.forEach(user => {
+    usersBody += `<tr>
+    <td><a href="#" data-id="${user.id}" class="deleteUser">Delete</a></td>
+    <td>${user.id}</td>
+    <td>${user.firstName}</td>
+    <td>${user.lastName}</td>
+    <td>${user.email}</td>
+    </tr>`
+  });
+  global.document.getElementById('users').innerHTML = usersBody;
+});
