00c37db4ad77e3678b652e2d2b60aac88d4d1ddb
Michael Brett <michael.brett.webdev@gmail.com>
2018-01-11 18:27:53 +0000

    Add an offline start mode

--- buildScripts/startMessage.js
+++ buildScripts/startMessage.js
@@ -1,3 +1,6 @@
+/* eslint-disable no-console */
 import chalk  from 'chalk';
 
-console.log(chalk.green('Starting app in dev mode...')); // eslint-disable-line no-console
+process.env.NODE_ENV === 'offline'
+  ? console.log(chalk.yellow('Starting app in offline mode...'))
+  : console.log(chalk.green('Starting app in dev mode...'));
